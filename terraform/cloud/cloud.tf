50a093b8fbc9c50e960441c6f872aabac9b3ef1e
Nathan R Valentine <nrvale00@gmail.com>
2017-05-17 11:01:31 -0700

    remove vars for AWS creds since they will be pulled in from shell env of TFE

--- terraform/cloud/cloud.tf
+++ terraform/cloud/cloud.tf
@@ -7,8 +7,6 @@ terraform {
 }
 
 provider "aws" {
-  access_key = "${var.aws_access_key_id}"
-  secret_key = "${var.aws_secret_access_key}"
   region     = "${var.aws_default_region}"
 }
 
--- terraform/ghe/_interface.tf
+++ terraform/ghe/_interface.tf
@@ -10,16 +10,3 @@ variable "ghe_org" {
 variable "ghe_token" {
   type = "string"
 }
-
-variable "aws_default_region" {
-  type    = "string"
-  default = "us-west-2"
-}
-
-variable "aws_secret_access_key" {
-  type = "string"
-}
-
-variable "aws_access_key_id" {
-  type = "string"
-}
